a6d0e1c3ade120e4a7ca6968625b2c3b826de180
Faridonov Ilnur <faridonovi3@gmail.com>
2026-02-02 21:26:14 +0300

    урок

---
+++ JavaScript/datatypes.js
@@ -0,0 +1,12 @@
+"use strikt"; //strikt - использовать строго
+
+// Площадь прямоугольника
+const width = Number(5);
+const heidht = Number(7.1)
+const area = width*heidht;
+console.log(area);
+
+const logic = Boolean(true == false);
+const str = String("hello");
+const megastr = String('${width} * ${heidth} = ${area}');
+console.log(megastr);
