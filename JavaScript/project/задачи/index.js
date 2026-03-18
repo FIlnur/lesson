6293fa07c92195b67056a41d3e643954a78f1555
Faridonov Ilnur <faridonovi3@gmail.com>
2026-03-18 21:24:08 +0300

    урок

--- JavaScript/project/задачи/index.js
+++ JavaScript/project/задачи/index.js
@@ -1,10 +1,27 @@
-const element = document.querySelector(".container");
+const trafficLight = document.createElement("div");
+trafficLight.classList.add("traffic-lights");
+const container = document.querySelector(".container");
+container.appendChild(trafficLight);
+const lights = [];
+for (let index = 0; index < 3; index++) {
+    lights.push(document.createElement("div"));
+    lights[index].classList.add("light");
+    trafficLight.appendChild(lights[index])
+};
 
-element.style.textAlign = "center";
-element.style.verticalAlign = "center";
-element.style.color = "black"
 
-const traficLigthElement = document.createElement("div");
-itemElement.container = Math.floor(Math.random() * 3);
-listElement.appendChild(traficLigthElement);
+lights[0].dataset.color = "red";
+
+lights[1].dataset.color = "yellow";
+
+lights[2].dataset.color = "green";
+
+let n = 0;
+
+setInterval(function () {
+    lights[n].style.backgroundColor = lights[n].dataset.color;
+    lights[n > 0 ? n - 1 : lights.length - 1].style.backgroundColor = "";
+    n = (n + 1) % lights.length;
+
+}, 1000);
 
