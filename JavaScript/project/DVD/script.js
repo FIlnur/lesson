9586723831af0610c9716f6dd07b84c72c0ca2d8
Faridonov Ilnur <faridonovi3@gmail.com>
2026-03-23 21:14:03 +0300

    урок

---
+++ JavaScript/project/DVD/script.js
@@ -0,0 +1,49 @@
+const image = document.querySelector(".image-container");
+const position = {x: 0, y: 0};
+const screenSizes = {
+    get height() {
+        return window.innerHeight;
+    },
+    get width() {
+        return window.innerWidth;
+    }
+};
+const speed = {x: 5, y: 5};
+let hueRotation = 0;
+const minHueRotation = 60;
+const maxHueRotation = 270;
+
+function render () {
+
+    if (position.x +speed.x < 0 ||
+        position.x + image.clientWidth + speed.x >= screenSizes.width
+    ) {
+        hueRotation =
+            Math.floor (
+                Math.random() * (maxHueRotation - minHueRotation) + minHueRotation,
+            ) % 360; 
+        image.style.filter = `hue-rotate(${hueRotation}deg)`;
+        position.x = speed.x > 0 ? screenSizes.width - image.clientWidth: 0;
+        speed.x *= -1;
+    } else {
+        position.x += speed.x;
+    }
+    image.style.left = `${position.x}px`;
+
+     if (position.y +speed.y < 0 ||
+        position.y + image.clientHeight + speed.y >= screenSizes.height
+    ) {
+        hueRotation =
+            Math.floor (
+                Math.random() * (maxHueRotation - minHueRotation) + minHueRotation,
+            ) % 360; 
+        image.style.filter = `hue-rotate(${hueRotation}deg)`;
+        position.y = speed.y > 0 ? screenSizes.height - image.clientHeight: 0;
+        speed.y *= -1;
+    } else {
+        position.y += speed.y;
+    }
+    image.style.top = `${position.y}px`;
+    requestAnimationFrame(render);
+};
+render ();
