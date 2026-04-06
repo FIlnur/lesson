00c70ea98ce45a2bd8e860aba843831eb5f12d82
Илнур <faridonovi3@gmail.com>
2026-04-06 21:19:57 +0300

    классы: компания, ковбои

--- JavaScript/oop/oop_basic.js
+++ JavaScript/oop/oop_basic.js
@@ -21,10 +21,7 @@ class Account {
         console.log(`Снято: ${amount}`)
     }
 }
-function test() {
-    return 1
-}
-const a = test()
+
 const account = new Account("Иван", 25000);
 console.log(account.balance);
 account.withdraw(1000);
@@ -45,11 +42,52 @@ class Employee {
     }
 }
 class Company {
-    constructor(employee) {
-        this.newEmployees =[];
-        this.employee = employee;
+    constructor() {
+        this.employees =[];
     }  
     hire(employee) {
+        this.employees.push(employee)
+    }
+    fire(employee) {
+        this.employees.splice(this.employees.indexOf(employee));
+    }
+    check(employee) {
+       for (const stateEmployee of this.employees) {
+            if (stateEmployee === employee) {
+                return true
+            }
 
+       } 
+       return false 
+       // return this.employees.includes(employee)
     }
 }
+const company = new Company()
+const employee = new Employee("Иван", "программист")
+company.hire(employee);
+console.log(company.check(employee))
+company.fire(employee);
+console.log(company.check(employee))
+
+// Создайте класс Cowboy(ковбой), который имеет здоровье,
+// количество урона и возможность стрельбы в другого ковбоя.
+// Сделайте, чтобы два ковбоя выстрелили друг в друга по очереди.
+
+class Cowboy {
+    constructor(name, health, damage) {
+        this.name = name;
+        this.health = health;
+        this.damage = damage;
+    }
+    shoot(cowboy) {
+        cowboy.health -= this.damage;
+    }
+}
+const cowboy1 = new Cowboy("Ivan", 100, 7);
+const cowboy2 = new Cowboy("Ilya", 100, 6);
+
+cowboy1.shoot(cowboy2)
+console.log(cowboy2.health)
+cowboy2.shoot(cowboy1)
+console.log(cowboy1.health)
+
