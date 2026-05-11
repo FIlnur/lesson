ed695806d0ebca9b67afd45bf893bf14d4d50d26
Ilnur <faridonovi3@gmail.com>
2026-05-11 21:27:30 +0300

    синхронизация

---
+++ notifier/notifier.js
@@ -0,0 +1,8 @@
+// при нажатии на кнопку синхронизация, появляется всплывающее окно
+// синхронизация началась
+
+class notifier {
+    constructor(element) {
+        this.element = element
+    };
+}
